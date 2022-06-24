e8af212dea3a923db51f391ba3ff1de139bcfa4a
Shashank Kaushik <kaushikshashank1998@gmail.com>
2022-06-24 22:01:39 +0530

    learning comments

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
+const _ = require("lodash");  // lodash docs https://lodash.com/docs/, npm install lodash
+                              // niche, _.lowercase me use kara hai
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,14 +20,13 @@ app.use(express.static("public"));                      // taki public folder ke
 let posts = [];
 
 app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+  res.render("home", { startingContent: homeStartingContent,   // focus on this "{}" (JS object, JS object are key-value pairs)
+    posts: posts                                               // passing this posts array to home.ejs
     });
 });
 
 app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+  res.render("about", {aboutContent: aboutContent});  // views me jake about.ejs run karega, aboutContent ko b bhej dega frontend pe
 });
 
 app.get("/contact", function(req, res){
@@ -34,31 +34,34 @@ app.get("/contact", function(req, res){
 });
 
 app.get("/compose", function(req, res){
-  res.render("compose");
+  res.render("compose");                     // views me jake compose.ejs show kar dega
 });
 
 app.post("/compose", function(req, res){
+  // creating a JS-object "post", jisko posts array me save karenge
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
   };
 
-  posts.push(post);
-
-  res.redirect("/");
+  posts.push(post);    // posts array me post append kar dia
+  res.redirect("/");   // post karte hi yaha "/" redirect ho jaega
 
 });
 
+// :postname is kind of i/p which will take whatever we enter
+// this is concept of Route parameters
 app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
-
+  const requestedTitle = _.lowerCase(req.params.postName);   // "req.params.postName" is a proper syntax 
+  
+  // checking if our requestedTitle matches any of the storedTitle inside posts array 
   posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
+    const storedTitle = _.lowerCase(post.title);  // using lodash, snakecase, canelcase, kababcase sabko lowercase kardega
 
     if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
+      res.render("post", {                // views me jake post.ejs call kar
+        title: post.title,                // post.title ko as "title" pass kar post.ejs me
+        content: post.content             // post.content ko as "content" pass kar post.ejs me
       });
     }
   });
